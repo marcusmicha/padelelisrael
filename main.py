458a59cc4c9143bb5deaf2d23e6730832d765d02
Michael Marcus <michael.marcus@touchcast.com>
2023-07-04 18:03:30 +0300

    fix for render deployment

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import datetime
+import time
 from scraper import Scraper
 from telegram import TelegramBot
 from helpers import pretty_availabilities
 
 def main():
     """ Main program """
+    print('lets start')
     today = datetime.datetime.now()
     bot = TelegramBot()
     scraper = Scraper(today)
-    availabilities = scraper.execute()
-    res = pretty_availabilities(availabilities)
-    bot.send_message(res)
+    while(True):
+        availabilities = scraper.execute()
+        res = pretty_availabilities(availabilities)
+        bot.send_message(res)
+        time.sleep(600)
     return 0
 
 if __name__ == "__main__":
